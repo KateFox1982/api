d7b1864fc4a8ab33aa41c82ddb05f955fd971237
kate <fox_kate@list.ru>
2022-09-11 18:53:51 +0400

    new commit

---
+++ Desktop/git_exercise/my_project/controller/controllers.go
@@ -0,0 +1,116 @@
+package controller
+
+import (
+	"database/sql"
+	"encoding/json"
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+	"log"
+	"strconv"
+
+	"my_project/model"
+	"net/http"
+)
+
+type UserCtrl struct {
+	users model.UserModel
+	DB    *sql.DB
+}
+
+//type User struct {
+//	Id   int    `json:"id"`
+//	Name string `json:"name"`
+//	Sale int    `json:"sale"`
+//}
+
+func NewUserCtrl([]model.User) *UserCtrl {
+
+	return &UserCtrl{}
+}
+
+func (usr *UserCtrl) Getusers(res http.ResponseWriter, req *http.Request) {
+
+	res.Header().Set("Content-Type", "application/json")
+	users, err := usr.users.Getusers()
+	if err != nil {
+		log.Fatal(err)
+	}
+	p := NewUserCtrl(users)
+	json.NewEncoder(res).Encode(&p)
+}
+
+func (usr *UserCtrl) GetSingleUser(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(req) // we are extracting 'id' of the Course which we are passing in the url
+
+	var id = params["id"]
+	s, err := strconv.Atoi(id)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusBadRequest)
+	}
+
+	p, err := usr.users.GetSingleUser(s)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	json.NewEncoder(res).Encode(&p)
+
+}
+func (usr *UserCtrl) CreateUser(res http.ResponseWriter, req *http.Request) {
+
+	res.Header().Set("Content-Type", "application/json")
+	//
+	//var param model.User
+	var user model.User
+	err := json.NewDecoder(req.Body).Decode(&user)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	m, err := usr.users.CreateUser(user.Name, user.Sale)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusBadRequest)
+		return
+	}
+	json.NewEncoder(res).Encode(&m)
+}
+func (usr *UserCtrl) UpdateUser(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "application/json")
+
+	var user model.User
+	err := json.NewDecoder(req.Body).Decode(&user)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusBadRequest)
+		return
+	}
+	users, err := usr.users.UpdateUser(user.ID, user.Name, user.Sale)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	json.NewEncoder(res).Encode(&users)
+
+}
+func (usr *UserCtrl) DeleteUser(res http.ResponseWriter, req *http.Request) {
+
+	res.Header().Set("Content-Type", "application/json")
+	//var user model.User
+	params := mux.Vars(req)
+	id := params["id"]
+	s, err := strconv.Atoi(id)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusBadRequest)
+	}
+	p, err := usr.users.DeleteUser(s)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	json.NewEncoder(res).Encode(p)
+
+}
---
+++ Desktop/git_exercise/my_project/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"database/sql"
+	"github.com/gorilla/mux"
+	"log"
+	"my_project/controller"
+	"net/http"
+)
+
+//var DB *sql.DB
+
+type UserDB struct {
+	users controller.UserCtrl
+}
+
+func main() {
+	DataSourceName := "user=fox password=123 dbname=fix sslmode=disable"
+	Db, err := sql.Open("postgres", DataSourceName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	us := &UserDB{
+		users: controller.UserCtrl{DB: Db},
+	}
+	router := mux.NewRouter()
+	router.HandleFunc("/user/{id}", us.GetSingleUser).Methods("GET")
+	router.HandleFunc("/users", us.Getusers).Methods("GET")
+	router.HandleFunc("/user", us.CreateUser).Methods("POST")
+	router.HandleFunc("/user/{id}", us.DeleteUser).Methods("DELETE")
+	router.HandleFunc("/user", us.UpdateUser).Methods("PUT")
+	log.Fatal(http.ListenAndServe("127.0.0.1:4000", router))
+
+}
+func (us *UserDB) Getusers(res http.ResponseWriter, req *http.Request) {
+	us.Getusers(res, req)
+}
+func (us *UserDB) GetSingleUser(res http.ResponseWriter, req *http.Request) {
+	us.GetSingleUser(res, req)
+}
+func (us *UserDB) CreateUser(res http.ResponseWriter, req *http.Request) {
+
+	us.CreateUser(res, req)
+}
+func (us *UserDB) DeleteUser(res http.ResponseWriter, req *http.Request) {
+	us.DeleteUser(res, req)
+}
+func (us *UserDB) UpdateUser(res http.ResponseWriter, req *http.Request) {
+	us.UpdateUser(res, req)
+}
