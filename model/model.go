e415776f424af510aa4a51e3f5438cf24af67d28
KateFox1982 <109987582+KateFox1982@users.noreply.github.com>
2022-09-14 21:21:10 +0400

    new commit

--- model/model.go
+++ model/model.go
@@ -6,6 +6,7 @@ import (
 	"log"
 )
 
+// структура User
 type User struct {
 	ID   int    `json:"id"`
 	Name string `json:"name"`
@@ -15,6 +16,8 @@ type UserModel struct {
 	DB *sql.DB
 }
 
+// конструктор модели возвращающий указатель на структуру UserModel
+
 func NewUserModel(DB *sql.DB) *UserModel {
 	return &UserModel{
 		DB: DB,
@@ -23,13 +26,16 @@ func NewUserModel(DB *sql.DB) *UserModel {
 
 //метод модели по получению всех пользователей из БД
 func (m *UserModel) Getusers() ([]User, error) {
+	//запрос возврата сроки выборки из таблицы значений
 	var rows, err = m.DB.Query("SELECT id, name, sale FROM Misha2")
 	if err != nil {
 		log.Fatal("Ошибка в выбора таблицы ")
 		return nil, err
 	}
 	defer rows.Close()
+	//инициализация массива структур User
 	users := []User{}
+	//получение данных из всей таблицы
 	for rows.Next() {
 		p := User{}
 		err := rows.Scan(&p.ID, &p.Name, &p.Sale)
@@ -37,15 +43,19 @@ func (m *UserModel) Getusers() ([]User, error) {
 			log.Fatal("Ошибка сканирования результата селекта ")
 			return nil, err
 		}
+		//добавление новых данных в массив структур
 		users = append(users, p)
 	}
+	//возврат массива структур и ошибки
 	return users, err
 }
 
 //метод модели по получению всех пользователей из БД по id
 func (m *UserModel) GetSingleUser(id int) (User, error) {
 	var p User
+	//запрос возврата сроки выборки из таблицы значений значений по id
 	row1 := m.DB.QueryRow("SELECT id, name, sale FROM Misha2 where id=$1", id)
+	// сканирование полученных результатов
 	err := row1.Scan(&p.ID, &p.Name, &p.Sale)
 	if err == sql.ErrNoRows {
 		err = fmt.Errorf("Нет такого id=%d", id)
@@ -57,9 +67,9 @@ func (m *UserModel) GetSingleUser(id int) (User, error) {
 
 //метод модели по созданию нового пользователя используя уникальный id
 func (m *UserModel) CreateUser(name string, sale int) (User, error) {
-
+	//инициализация данных переданных из контроллера в структуру User
 	var user = User{Name: name, Sale: sale}
-
+	//создание нового значения используя данные переданных из контроллера с уникальным id
 	err := m.DB.QueryRow("INSERT INTO Misha2 (name, sale) VALUES($1,$2) returning id", user.Name, user.Sale).Scan(&user.ID)
 	if err != nil {
 		log.Fatal("Ошибка создания строки")
@@ -70,8 +80,10 @@ func (m *UserModel) CreateUser(name string, sale int) (User, error) {
 
 //метод модели по изменению конкретного пользователя из БД
 func (m *UserModel) UpdateUser(id int, name string, sale int) (User, error) {
+	//инициализация данных переданных из контроллера в структуру User
 	user := User{ID: id, Name: name, Sale: sale}
 	fmt.Println("Печать из модели", id, name, sale)
+	//изменения в БД значений при совпадении id
 	_, err := m.DB.Exec("update Misha2 set name =$1, sale= $2 where id = $3", user.Name, user.Sale, user.ID)
 	if err != nil {
 		err = fmt.Errorf("Нет такого id=%d", user.ID)
@@ -83,6 +95,7 @@ func (m *UserModel) UpdateUser(id int, name string, sale int) (User, error) {
 ////метод модели по удалению конкретного пользователя из БД по id
 func (m *UserModel) DeleteUser(id int) (User, error) {
 	var s *string
+	//нахождение строки с id переданного из контроллера
 	row1 := m.DB.QueryRow("SELECT FROM Misha2 where id=$1", id)
 	err := row1.Scan(&s)
 	if err == sql.ErrNoRows {
@@ -90,6 +103,7 @@ func (m *UserModel) DeleteUser(id int) (User, error) {
 		return User{}, err
 	} else {
 		var k string
+		//удаление строки в случае нахождения данной строки
 		row := m.DB.QueryRow("DELETE  FROM Misha2 where id=$1", id)
 		err = row.Scan(&k)
 		if err == sql.ErrNoRows {
