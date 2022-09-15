2a4bb3be644ceee632b28c1b5042c6559b68cab5
KateFox1982 <109987582+KateFox1982@users.noreply.github.com>
2022-09-15 22:59:31 +0400

    new commit

--- controller/controllers.go
+++ controller/controllers.go
@@ -7,7 +7,6 @@ import (
 	"fmt"
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
-	"log"
 	"strconv"
 
 	"my_project/model"
@@ -34,9 +33,9 @@ func (usr *UserCtrl) Getusers(res http.ResponseWriter, req *http.Request) {
 	//обращение к методу модели Getusers
 	users, err := usr.users.Getusers()
 	if err != nil {
-		log.Printf("Ошибка выполнеия функции получения информации о всех пользователях: %s", err)
-		fmt.Fprintf(res, "Ошибка выполнеия функции получения информации о всех пользователях: %s", err)
-
+		m := "Ошибка выполнеия функции получения информации о всех пользователях: %s"
+		fmt.Println(m, err)
+		fmt.Fprintf(res, m, err)
 		return
 	}
 	//кодирование в json результата выполнения метода и передача в пакет main
@@ -54,15 +53,17 @@ func (usr *UserCtrl) GetSingleUser(res http.ResponseWriter, req *http.Request) {
 	//конвертация string в int
 	s, err := strconv.Atoi(id)
 	if err != nil {
-		fmt.Println("Ошибка перевода id из string в int ", err)
-		fmt.Fprintf(res, "Ошибка перевода id из string в int ", err)
+		m := "Ошибка перевода id из string в int "
+		fmt.Println(m, err)
+		fmt.Fprintf(res, m, err)
 		return
 	}
 	//передача парметра id методу модели GetSingleUser
 	p, err := usr.users.GetSingleUser(s)
 	if err != nil {
-		fmt.Println("Ошибка выполнения функции выбора по id: ", err)
-		fmt.Fprintf(res, "Ошибка выполнения функции выбора по id: ", err)
+		m := "Ошибка выполнения функции выбора по id: "
+		fmt.Println(m, err)
+		fmt.Fprintf(res, m, err)
 		return
 	}
 	//кодирование в json результата выполнения метода и передача в пакет main
@@ -79,15 +80,17 @@ func (usr *UserCtrl) CreateUser(res http.ResponseWriter, req *http.Request) {
 	//декорирование тела запроса в структуру
 	err := json.NewDecoder(req.Body).Decode(&user)
 	if err != nil {
-		fmt.Println("Ошибка чтения информации для создания новой записи : ", err)
-		fmt.Fprintf(res, "Ошибка чтения информации для создания новой записи : ", err)
+		m := "Ошибка чтения информации для создания новой записи : "
+		fmt.Println(m, err)
+		fmt.Fprintf(res, m, err)
 		return
 	}
 	//передача парметров запроса методу модели CreateUser
 	m, err := usr.users.CreateUser(user.Name, user.Sale)
 	if err != nil {
-		fmt.Println("При выполнении функции создания возникла ошибка: ", err)
-		fmt.Fprintf(res, "При выполнении функции создания возникла ошибка: ", err)
+		m := "При выполнении функции создания возникла ошибка: "
+		fmt.Println(m, err)
+		fmt.Fprintf(res, m, err)
 		return
 	}
 	//кодирование в json результата выполнения метода и передача в пакет main
@@ -103,15 +106,17 @@ func (usr *UserCtrl) UpdateUser(res http.ResponseWriter, req *http.Request) {
 	//декорирование тела запроса в структуру
 	err := json.NewDecoder(req.Body).Decode(&user)
 	if err != nil {
-		fmt.Println("Ошибка маршаллинга данных для изменения", err)
-		fmt.Fprintf(res, "Ошибка маршаллинга данных для изменения", err)
+		m := "Ошибка маршаллинга данных для изменения"
+		fmt.Println(m, err)
+		fmt.Fprintf(res, m, err)
 		return
 	}
 	//передача парметров запроса методу модели UpdateUser
 	users, err := usr.users.UpdateUser(user.ID, user.Name, user.Sale)
 	if err != nil {
-		fmt.Println("При изменении что то пошло не так:", err)
-		fmt.Fprintf(res, "При изменении что то пошло не так:", err)
+		m := "При изменении что то пошло не так:"
+		fmt.Println(m, err)
+		fmt.Fprintf(res, m, err)
 		return
 	}
 	//кодирование в json результата выполнения метода и передача в пакет main
@@ -128,15 +133,17 @@ func (usr *UserCtrl) DeleteUser(res http.ResponseWriter, req *http.Request) {
 	//конвертация string в int
 	s, err := strconv.Atoi(id)
 	if err != nil {
-		fmt.Println("Неудачно выполнен перевод id bp string в int ", err)
-		fmt.Fprintf(res, "Неудачно выполнен перевод id bp string в int ", err)
+		m := "Неудачно выполнен перевод id bp string в int "
+		fmt.Println(m, err)
+		fmt.Fprintf(res, m, err)
 		return
 	}
 	//передача парметра id методу модели GetSingleUser
 	p, err := usr.users.DeleteUser(s)
 	if err != nil {
-		fmt.Println("Не удачно удалилось ", err)
-		fmt.Fprintf(res, "Не удачно удалилось ", err)
+		m := "Не удачно удалилось "
+		fmt.Println(m, err)
+		fmt.Fprintf(res, m, err)
 		return
 	}
 	//кодирование в json результата выполнения метода и передача в пакет main
