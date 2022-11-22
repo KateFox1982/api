4c3b605ad0a7496bbd9ca0d0e9a0906b0cedc11a
KateFox1982 <109987582+KateFox1982@users.noreply.github.com>
2022-11-22 10:49:44 +0400

    new commit

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 
 func main() {
 	//парметры БД: имя пользователя, пароль, имя БД, использование SSL
-	DataSourceName := "user=fox password=123 dbname=fix sslmode=disable"
+	DataSourceName := "user=fox password=123 dbname=fix sslmode=disable "
 	//соединение с БД postgres
 	DB, err := sql.Open("postgres", DataSourceName)
 	//err ошибка соединения
@@ -27,37 +27,58 @@ func main() {
 	//контроллера с аргументом DB, прием-передача параметров функции контроллера Getusers
 	router.HandleFunc("/users", func(res http.ResponseWriter, req *http.Request) {
 		//userCtrl := controller.NewUserCtrl()
-		userCtrl := controller.NewUserCtrl(DB)
+		userCtrl := controller.NewUserController(DB)
 		userCtrl.Getusers(res, req)
 	}).Methods("GET")
 	//router.HandleFunc регистрация второго маршрута, с URL оканчивающимся на "/user и параметром id, который пользователь указывает в URL,
 	//и методом GET, созадет новый экземпляр конструктора
 	//контроллера с аргументом DB, прием-передача параметров функции контроллера GetSingleUser
 	router.HandleFunc("/user/{id}", func(res http.ResponseWriter, req *http.Request) {
-		userCtrl := controller.NewUserCtrl(DB)
+		userCtrl := controller.NewUserController(DB)
 		userCtrl.GetSingleUser(res, req)
 	}).Methods("GET")
 	//router.HandleFunc регистрация третьего маршрута, с URL оканчивающимся на "/user и параметром id, который пользователь указывает в URL,
 	//и методом DELETE, созадет новый экземпляр конструктора
 	//контроллера с аргументом DB, прием-передача параметров функции контроллера DeleteUser
 	router.HandleFunc("/user/{id}", func(res http.ResponseWriter, req *http.Request) {
-		userCtrl := controller.NewUserCtrl(DB)
+		userCtrl := controller.NewUserController(DB)
 		userCtrl.DeleteUser(res, req)
 	}).Methods("DELETE")
 	//router.HandleFunc регистрация третьего маршрута, с URL оканчивающимся на "/user ,
 	//и методом PUT, созадет новый экземпляр конструктора
 	//контроллера с аргументом DB, прием-передача параметров функции контроллера UpdateUser
 	router.HandleFunc("/user", func(res http.ResponseWriter, req *http.Request) {
-		userCtrl := controller.NewUserCtrl(DB)
+		userCtrl := controller.NewUserController(DB)
 		userCtrl.UpdateUser(res, req)
 	}).Methods("PUT")
 	//router.HandleFunc регистрация третьего маршрута, с URL оканчивающимся на "/user ,
 	//и методом POST, созадет новый экземпляр конструктора
 	//контроллера с аргументом DB, прием-передача параметров функции контроллера CreateUser
 	router.HandleFunc("/user", func(res http.ResponseWriter, req *http.Request) {
-		userCtrl := controller.NewUserCtrl(DB)
+		userCtrl := controller.NewUserController(DB)
 		userCtrl.CreateUser(res, req)
 	}).Methods("POST")
+	//router.HandleFunc регистрация первого маршрута, с URL оканчивающимся на "/documents" и методом GET, созадет новый экземпляр конструктора
+	//контроллера с аргументом DB, прием-передача параметров функции контроллера Getusers
+	router.HandleFunc("/documents", func(res http.ResponseWriter, req *http.Request) {
+		//userCtrl := controller.NewUserCtrl()
+		userCtrl := controller.NewDocumentModel(DB)
+		userCtrl.GetDocuments(res, req)
+	}).Methods("GET")
+	//router.HandleFunc регистрация второго маршрута, с URL оканчивающимся на "/module и параметром id, который пользователь указывает в URL,
+	//и методом GET, созадет новый экземпляр конструктора
+	//контроллера с аргументом DB, прием-передача параметров функции контроллера GetModuleById
+	router.HandleFunc("/module/{id}", func(res http.ResponseWriter, req *http.Request) {
+		userCtrl := controller.NewModuleModel(DB)
+		userCtrl.GetModuleById(res, req)
+	}).Methods("GET")
+	//router.HandleFunc регистрация второго маршрута, с URL оканчивающимся на "/error и параметром id, который пользователь указывает в URL,
+	//и методом GET, созадет новый экземпляр конструктора
+	//контроллера с аргументом DB, прием-передача параметров функции контроллера GetErrorById
+	router.HandleFunc("/error/{id}", func(res http.ResponseWriter, req *http.Request) {
+		userCtrl := controller.NewErrorModel(DB)
+		userCtrl.GetErrorById(res, req)
+	}).Methods("GET")
 	//ListenAndServe запуск http -сервером с адресом 127.0.0.1:4000, и обработчиком router
 	log.Fatal(http.ListenAndServe("127.0.0.1:4000", router))
 }
